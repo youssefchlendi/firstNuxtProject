6bfcbbd22ccbf332a14c072bcacf9966a6b7bda5
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-07-22 09:14:32 +0100

    update gitIgnore and set router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  router: {
+    base: '/firstNuxtProject/'
+  },
   head: {
     titleTemplate: '%s - firstNuxtProject',
     title: 'firstNuxtProject',
