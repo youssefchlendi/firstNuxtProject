160a77e816624db66bacf2145fa6c205607c4179
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-07-24 18:54:51 +0100

    rollback base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,7 @@ export default {
       laravelSanctum: {
         provider: 'laravel/sanctum',
         tokenType: 'Bearer',
-        url: 'https://smurfsdev.tn/back-end/public',
+        url: 'http://localhost:8000',
         endpoints: {
           login: { url: '/api/login' },
           user: { url: '/api/profile' },
@@ -76,7 +76,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://smurfsdev.tn/back-end/public/api/',
+    baseURL: 'http://localhost:8000/api',
     credentials: true
 
   },
