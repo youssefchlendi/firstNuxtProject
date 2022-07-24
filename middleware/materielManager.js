25e2ce1cc59441cd33f3761e9d6aff678b3151ad
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-07-24 12:50:36 +0100

    fix materielManager middleware

--- middleware/materielManager.js
+++ middleware/materielManager.js
@@ -1,5 +1,5 @@
 export default function ({ store, error }) {
-  if (store.state.auth.user.responsability === null && store.state.auth.user.responsability !== 'materiel-manager') {
+  if (store.state.auth.user.responsability === null || store.state.auth.user.responsability !== 'materiel-manager') {
     error({ statusCode: 401, message: 'لست مخول للدخول لهذه الصفحة' })
   }
 }
